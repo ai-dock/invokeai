04f949589efd45c5bfc076981f5f7b8bbf60d4d4
Rob Ballantyne <rob@dynamedia.uk>
2024-06-27 22:05:59 +0100

    Fix ROCm torch version

--- build/COPY_ROOT_0/opt/ai-dock/bin/build/layer0/amd.sh
+++ build/COPY_ROOT_0/opt/ai-dock/bin/build/layer0/amd.sh
@@ -7,11 +7,12 @@ build_amd_main() {
 
 build_amd_install_torch() {
     "$INVOKEAI_VENV_PIP" install --no-cache-dir \
-        numpy'<2' \
+        numpy'<2'
+    "$INVOKEAI_VENV_PIP" install --no-cache-dir \
         torch==${PYTORCH_VERSION} \
         torchvision \
         torchaudio \
-        --extra-index-url=https://download.pytorch.org/whl/rocm${ROCM_VERSION}
+        --index-url=https://download.pytorch.org/whl/rocm${ROCM_VERSION}
 }
 
 build_amd_main "$@"
